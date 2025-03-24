2625cbe39c0db22f3878cbec70ac41844ac016df
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2025-03-24 22:56:17 +0600

    test

--- product/models.py
+++ product/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     category = models.CharField(max_length=50, unique=True, verbose_name='Категория')
+    image = models.ImageField(upload_to='product/', blank=True, null=True)
 
     def __str__(self):
         return self.category
