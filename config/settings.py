0a7a30427fc78368ed0dfa0fe0d2c234050c0b74
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2025-03-29 13:04:13 +0600

    test

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.49.244.251', 'promgrad.pp.ua']
+ALLOWED_HOSTS = ['promgrad.pp.ua']
 
 
 # Application definition
--- config/urls.py
+++ config/urls.py
@@ -2,19 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework import permissions
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/product/', include('product.urls')),
     
-    # Генерация схемы API
+
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     
-    # Swagger UI
+
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     
-    # ReDoc UI
+
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
